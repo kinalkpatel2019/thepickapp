b93844845bea8c7c654d62d509e3ebf9e624345b
kinalkpatel2019 <58947539+kinalkpatel2019@users.noreply.github.com>
2020-05-16 16:26:44 +0530

    Delete Old Files

--- application/views/template/beforelogin/index.php
+++
@@ -1,5 +0,0 @@
-<?php $this->load->view('template/beforelogin/_header.php') ; ?>
-<div class="container">
-  <?php $this->load->view($main_content); ?>
-</div>
-<?php $this->load->view('template/beforelogin/_footer.php') ; ?>
